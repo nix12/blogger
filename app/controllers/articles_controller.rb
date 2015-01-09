a865fcdf7a47865130872f61105f842b198e35b2
Mario Goodwin <mr.goodwin@outlook.com>
2015-01-08 20:20:18 -0500

    Updated the controllers and the view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,37 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update_attributes(article_params)
+		flash.notice = "Article '#{@article.title}' Updated"
+		redirect_to article_path(@article)
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.create(article_params)
+		flash.notice = "Article '#{@article.title}' has been created."
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id]).destroy
+		flash.notice = "Article '#{@article.title}' has been deleted."	
+		redirect_to articles_path	
+	end
+
+	private
+
+		def article_params
+			params.require(:article).permit(:title, :body)
+		end
 end
